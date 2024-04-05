dcd5a9b6e8a020b6158d43da16b291eecb914153
Bruno Lucas <brunnnolucas@gmail.com>
2024-04-05 11:43:23 -0300

    adicionando comando start

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,9 @@ const server = Fastify({ logger: true });
 const Start = async () => {
   await server.register(Cors);
   await server.register(Routes);
-
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3333;
   try {
-    await server.listen({ port: 3333 });
+    await server.listen({ port: port });
   } catch (err) {
     process.exit(1);
   }
